ffe1616bb11d4b04171aef742650bc83d1575cf1
Rafael Dos Santos <raphael_979@hotmail.com>
2019-09-09 18:08:34 -0300

    Dentro da psta enums foi criado a Classe Pedido Status com pagamentoPedente,Processando,Enviado,Entregue

---
+++ Entidades/Enums/PedidoStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnumeraçõesEnum.Entidades.Enums
+{
+    enum PedidoStatus:int
+    {
+        PagamentoPedente=0,
+        Processando=1,
+        Enviado=2,
+        Entregue=3
+    }
+}
